5486cb87452a488aa40dd5a63978504564076232
nocox <ddnd7nocox@gmail.com>
2020-04-05 11:12:53 +0900

    sample分岐用のページを作成 (home)

--- bt-backend/src/main/java/jp/nocox/btraining/DemoController.java
+++ bt-backend/src/main/java/jp/nocox/btraining/DemoController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DemoController {
+
+    @GetMapping("/")
+    public String home(Model model){
+        return "home";
+    }
+
     @GetMapping("/sample")
     public String sample(Model model){
         model.addAttribute("message", "ここにメッセージを入れるよ");
